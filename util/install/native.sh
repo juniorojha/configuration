63a21c7a30d286c34488ac68a5d4f67d5ec81dc0
Gaurav <ojha.iiitm@gmail.com>
2019-05-22 12:09:36 +0530

    Optimizing configuration repo clone

--- util/install/native.sh
+++ util/install/native.sh
@@ -147,9 +147,9 @@ CONFIGURATION_VERSION=${CONFIGURATION_VERSION-$OPENEDX_RELEASE}
 ## Clone the configuration repository and run Ansible
 ##
 cd /var/tmp
-git clone https://github.com/edx/configuration
+git clone https://github.com/juniorojha/configuration --depth 1 --branch $CONFIGURATION_VERSION
 cd configuration
-git checkout $CONFIGURATION_VERSION
+# git checkout $CONFIGURATION_VERSION
 git pull
 
 ##
